cb012c5cba1b707e9a072188b4eeb2adb46f1393
xav921 <xav7470@gmail.com>
2019-09-11 14:47:47 -0600

    Add salutation method to construct and return a salutation string.

--- src/deepdive/prework/HelloWorld.java
+++ src/deepdive/prework/HelloWorld.java
@@ -3,11 +3,15 @@ package deepdive.prework;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    salute((args.length > 0) ? args[0] : "World");
+    salute(salutation(args));
   }
 
-  private static void salute(String target) {
-    System.out.println("Hello, " + target + "!");
+  private static String salutation(String[] args) {
+    return "Hello, " + ((args.length > 0) ? args[0] : "World");
+  }
+
+  private static void salute(String salutation) {
+    System.out.println(salutation);
   }
 
 }
